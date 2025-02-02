2c18623f7c7cdd60a0bf68007590e207c5dcb5a0
yolandawu <yolandawu>
2025-02-01 23:59:16 -0500

    bg style change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
       <ThemeProvider theme={darkTheme}>
           <CssBaseline />
             <main className="p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] text-center mt-20">
+                <h1 style={{fontSize:28, marginBottom:'40px'}}>Do we need a project name here?</h1>
                 <Login onSuccess={handleLoginSuccess} onFail={handleLoginFail} />
             </main>
       </ThemeProvider>
