5723570e190222231d79aa637f1fb6ae60112dc4
yolandawu <yolandawu>
2025-02-02 00:19:37 -0500

    add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,10 @@ export default function Home() {
       <ThemeProvider theme={darkTheme}>
           <CssBaseline />
             <main className="p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] text-center mt-20">
-                <h1 style={{fontSize:28, marginBottom:'40px'}}>Do we need a project name here?</h1>
+                <p style={{fontSize:28, marginBottom:'40px'}}>
+                    Welcome to the
+                    <br/>Intelligent Digital Asset Assistant
+                </p>
                 <Login onSuccess={handleLoginSuccess} onFail={handleLoginFail} />
             </main>
       </ThemeProvider>
