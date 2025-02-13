36f994e625c7797408dc06c08267327762822a89
yolandawu <yolandawu>
2025-02-13 17:22:22 -0500

    add loading before getting resp from server api

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,19 +1,21 @@
 'use client'
-import {useState, useRef, useEffect} from 'react'
+import {useState, useRef, useEffect, JSX} from 'react'
 import TextField from '@mui/material/TextField';
-import {Button,Alert} from '@mui/material';
+import {Button} from '@mui/material';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import DialogueChatBot from "@/app/components/DialogueChatBot";
 import DialogueUser from "@/app/components/DialogueUser";
 import {sendMessage} from '@/app/api/handler'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Chat() {
     const [userInput, setUserInput] = useState("");
-    const [messages, setMessages] = useState<{ type: 'user' | 'bot', text: string }[]>([]);
-    const [alertMessage, setAlertMessage] = useState<string>('');
-    const [showAlert, setShowAlert] = useState(false);
+    const [messages, setMessages] = useState<{ type: 'user' | 'bot', text: string | JSX.Element, loading?: boolean }[]>([]);
     const [isSubmitting, setIsSubmitting] = useState(false);
     const messagesContainerRef = useRef<HTMLDivElement | null>(null);
+    const alertMessage = 'Oooops! Network error, please try again later.'
 
     // function to scroll to bottom
     const scrollToBotton = () => {
@@ -36,39 +38,50 @@ export default function Chat() {
         setUserInput("");
         setMessages((prev) => [...prev, { type: 'user', text: userInput }]);
 
+        // append a "thinking" message from the bot
+        setMessages((prev) =>  [...prev,
+            {
+                type: 'bot',
+                text: (
+                    <div className="flex px-3 flex-1 my-3">
+                        <FontAwesomeIcon icon={faSpinner} className="animate-spin text-gray-500 pl-1 pr-2 pt-1" />
+                        <a className='ml-2'>Thinking...</a>
+                    </div>
+                ),
+                loading: true
+            }]);
+
         sendMessage(userInput)
             .then((res)=>{
                 setIsSubmitting(false)
                 if(res && res.agent_result) {
                     const botReply = res.agent_result || "Sorry, I didn't understand that."; // Ensure fallback
-                    setMessages((prev) => [...prev, { type: 'bot', text: botReply }]);
+                    // Replace "Thinking..." with the actual bot reply
+                    setMessages((prev) =>
+                        prev.map((msg) =>
+                            msg.loading ? { ...msg, text: botReply, loading: false } : msg
+                        )
+                    );
                 }
                 else {
-                    setAlertMessage('Oooops! Network error, please try again later.');
-                    setShowAlert(true)
+                    setMessages((prev) => prev.filter((msg) => !msg.loading))
+                    setMessages((prev) =>[...prev, { type: 'bot', text: alertMessage, loading: false }]);
                 }
 
             })
             .catch(()=>{
-                setAlertMessage('Oooops! Network error, please try again later.');
-                setShowAlert(true)
                 setIsSubmitting(false)
+                setMessages((prev) => prev.filter((msg) => !msg.loading))
+                setMessages((prev) =>[...prev, { type: 'bot', text: alertMessage, loading: false }]);
             })
     };
 
     return (
         <div className="relative text-left flex flex-col relateive" style={{height:'96%'}}>
-            {showAlert && (
-                <div className="w-1/4 fixed right-2 top-5">
-                    <Alert severity="warning" onClose={() => setShowAlert(false)}>
-                        {alertMessage}
-                    </Alert>
-                </div>
-                )}
             <div className='py-5 flex-1 overflow-scroll' ref={messagesContainerRef}>
                 {messages.map((msg, index) =>
                     msg.type === 'user' ?
-                        <DialogueUser key={index} userReply={msg.text} /> :
+                        <DialogueUser key={index} userReply={msg.text.toString()} /> :
                         <DialogueChatBot key={index} chatbotReply={msg.text} />
                 )}
             </div>
--- src/app/components/DialogueChatBot.tsx
+++ src/app/components/DialogueChatBot.tsx
@@ -1,18 +1,23 @@
 'use client'
 import iconChatbot from '../images/icon-bot.png'
 import ReactMarkdown from "react-markdown";
+import {JSX} from "react";
 
-export default function DialogueChatBot({chatbotReply}: {chatbotReply: string}) {
+export default function DialogueChatBot({chatbotReply}: { chatbotReply: string | JSX.Element }) {
 
     return (
         <div className='flex flex-row w-full pb-1'>
             <div className='flex items-left p-2  w-11/12'>
                 <img style={{width:'45px', height:'45px'}} src={iconChatbot.src} alt=''/>
-                <ReactMarkdown
-                    className='px-3 flex-1 my-3 reactMarkDown'
-                >
-                    {chatbotReply.replace(/\n/gi, " &nbsp;\n\r ")}
-                </ReactMarkdown>
+                {typeof chatbotReply === 'string' ? (
+                    <ReactMarkdown
+                        className='px-3 ml-2 flex-1 my-3 reactMarkDown'
+                    >
+                        {chatbotReply.replace(/\n/gi, " &nbsp;\n\r ")}
+                    </ReactMarkdown>
+                    ) : (
+                    chatbotReply
+                )}
             </div>
         </div>
     )
